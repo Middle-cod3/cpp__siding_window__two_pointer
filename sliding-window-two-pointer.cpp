7869b53badf095f03fd8a9ca42597ada0292a166
Middle-cod3 <92102240+Middle-cod3@users.noreply.github.com>
2024-08-23 17:51:07 +0530

    added minWindowO

--- sliding-window-two-pointer.cpp
+++ sliding-window-two-pointer.cpp
@@ -728,8 +728,69 @@ string minWindow(string s, string t)
 }
 
 // Optimal ---------->
-// TC :
-// SC :
+// Time Complexity:O(2N + M ), where N is the size of the string s and M is the size of the string t.
+// Space Complexity: O(256) . Hash array to store frequency of all the characters.
+string minWindowO(string s, string t)
+{
+
+    /* Variable to store the minimum
+    length of substring found */
+    int minLen = INT_MAX;
+
+    /* Variable to store the starting index
+    of the minimum length substring */
+    int sIndex = -1;
+
+    /* Array to count frequencies
+    of characters in string t*/
+    int hash[256] = {0};
+
+    // Count the frequencies of characters in t
+    for (char c : t)
+    {
+        hash[c]++;
+    }
+
+    int count = 0;
+    int l = 0, r = 0;
+
+    // Iterate through current window
+    while (r < s.size())
+    {
+        // Include the current character in the window
+        if (hash[s[r]] > 0)
+        {
+            count++;
+        }
+        hash[s[r]]--;
+
+        /* If all characters from t
+        are found in current window */
+        while (count == t.size())
+        {
+
+            /* Update minLen and sIndex
+            if current window is smaller */
+            if (r - l + 1 < minLen)
+            {
+                minLen = r - l + 1;
+                sIndex = l;
+            }
+
+            // Remove leftmost character from window
+            hash[s[l]]++;
+            if (hash[s[l]] > 0)
+            {
+                count--;
+            }
+            l++;
+        }
+        r++;
+    }
+
+    // Return minimum length substring from s
+    return (sIndex == -1) ? "" : s.substr(sIndex, minLen);
+}
 
 /*
 10. Max Consecutive Ones III
